27307374837fac60aee296d083c42486938bcb0c
Janhavi Patil <patil.janhavi1207@gmail.com>
2024-04-05 22:31:02 +0530

    feat: add google auth

---
+++ src/components/globals/header.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { ModeToggle } from "../mode-toggle";
+import { Button } from "../ui/button";
+
+export function Header() {
+  const session = useSession();
+  return (
+    <header>
+      <div>
+        {session.data ? (
+          <Button onClick={() => signOut()}>Sign Out</Button>
+        ) : (
+          <Button onClick={() => signIn("google")}>Sign In</Button>
+        )}
+        {session.data?.user?.name}
+        <ModeToggle />
+      </div>
+    </header>
+  );
+}
