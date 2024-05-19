ce0737f4c58fedc03eaff903ac0ad6b443f5e92e
Janhavi Patil <patil.janhavi1207@gmail.com>
2024-05-19 23:07:41 +0530

    feat: add room page

--- src/data-access/rooms.ts
+++ src/data-access/rooms.ts
@@ -1,9 +1,20 @@
-import { db } from '@/db'
-import { unstable_noStore } from 'next/cache';
+import { db } from "@/db";
+import { room } from "@/db/schema";
+import { eq } from "drizzle-orm";
+import { unstable_noStore } from "next/cache";
 
 export async function getRooms() {
-    unstable_noStore();
-    const rooms = await db.query.room.findMany();
+  unstable_noStore();
+  const rooms = await db.query.room.findMany();
 
-    return rooms;
+  return rooms;
+}
+
+export async function getRoom(roomId: string) {
+  unstable_noStore();
+  return await db.query.room.findFirst({
+    where: eq(room?.id, roomId),
+  });
+
+
 }
